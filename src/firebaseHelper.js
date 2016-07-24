cf5a133c6eccb35dea0f9c6e895dfeff859c268d
Andrew Jack <me@andrewjack.uk>
2016-07-24 16:57:03 +0100

    auth

--- src/firebaseHelper.js
+++ src/firebaseHelper.js
@@ -5,14 +5,16 @@ import conf from './config';
 
 const gcs = gcloud.storage({
   projectId: conf.get('firebase:projectId'),
-  serviceAccount: conf.get('firebase:serviceAccount'),
+  keyFilename: conf.get('firebase:keyFilename'),
 });
 const bucket = gcs.bucket(conf.get('firebase:storageBucket'));
 
 export default function uploadFile(fileName) {
-  bucket.upload(`./captures/${fileName}`, (err, file) => {
+  bucket.upload(`captures/${fileName.replace('~', '')}`, (err, file) => {
     if (!err) {
       console.log(`File saved as ${file}`);
+    } else {
+      console.warn(`Error: ${err}`);
     }
   });
 }
